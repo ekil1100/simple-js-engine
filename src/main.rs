c5b9f95b0928e13ad707691afa8ac688e4967256
Like <like@ekil.sh>
2023-12-28 00:05:32 +0800

    .

--- src/main.rs
+++ src/main.rs
@@ -1,70 +1,71 @@
-#[derive(Debug, PartialEq)]
-enum TokenType {
-    Keyword,
-    Identifier,
-    Operator,
-    Number,
+#[derive(Debug)]
+enum Token {
+    Keyword(String),
+    Identifier(String),
+    Number(i32),
+    Operator(String),
     Semicolon,
+    Dot,
+    Parenthesis(char),
+    // 其他标记...
 }
 
-#[derive(Debug)]
-struct Token {
-    token_type: TokenType,
-    value: String,
+enum State {
+    Initial,
+    Identifier,
+    Number,
+    Operator,
+    // 其他状态...
 }
 
 fn lex(input: &str) -> Vec<Token> {
     let mut tokens = Vec::new();
-    let mut chars = input.chars().peekable();
+    let mut current_state = State::Initial;
+    let mut current_token = String::new();
 
-    while let Some(&ch) = chars.peek() {
-        let token = if ch.is_alphabetic() {
-            let mut value = String::new();
-            while let Some(&ch) = chars.peek() {
-                if ch.is_alphanumeric() {
-                    value.push(ch);
-                    chars.next();
+    for ch in input.chars() {
+        match current_state {
+            State::Initial => {
+                current_state = State::Identifier;
+            },
+            State::Identifier => {
+                if ch.is_alphabetic() {
+                    current_token.push(ch);
                 } else {
-                    break;
+                    tokens.push(Token::Identifier(current_token.clone()));
+                    current_token.clear();
+                    current_state = State::Operator;
                 }
-            }
-
-            match value.as_str() {
-                "let" => Token { token_type: TokenType::Keyword, value },
-                _ => Token { token_type: TokenType::Identifier, value },
-            }
-        } else if ch.is_digit(10) {
-            let mut value = String::new();
-            while let Some(&ch) = chars.peek() {
-                if ch.is_digit(10) {
-                    value.push(ch);
-                    chars.next();
+            },
+            State::Number => {
+                if ch.is_numeric() {
+                    current_token.push(ch);
                 } else {
-                    break;
+                    tokens.push(Token::Number(current_token.parse().unwrap()));
+                    current_token.clear();
+                    current_state = State::Initial;
                 }
-            }
-
-            Token { token_type: TokenType::Number, value }
-        } else if ch == '=' {
-            chars.next();
-            Token { token_type: TokenType::Operator, value: ch.to_string() }
-        } else if ch == ';' {
-            chars.next();
-            Token { token_type: TokenType::Semicolon, value: ch.to_string() }
-        } else {
-            // 简化处理：忽略其他字符
-            chars.next();
-            continue;
-        };
-
-        tokens.push(token);
+            },
+            // 其他状态的处理...
+        }
     }
-
     tokens
 }
 
+
+// fn parse(tokens: Vec<Token>) -> AST {
+//     // 将标记转换为 AST
+// }
+
+// fn execute(ast: AST, env: &mut Environment) {
+//     // 执行 AST，更新环境
+// }
+
 fn main() {
-    let input = "let x = 5;";
+    let input = "let x = 5; console.log(x);";
     let tokens = lex(input);
-    println!("{:?}", tokens);
+    // let ast = parse(tokens);
+    // let mut env = Environment::new();
+    // execute(ast, &mut env);
+    print!("{:?}", tokens)
 }
